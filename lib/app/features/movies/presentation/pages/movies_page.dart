bd41f861559ec192ae8994e1fb346746823718ca
guilhermeFCarvalho <gui.carvalho021@gmail.com>
2024-04-18 21:47:37 -0300

    chore: remove mock list

--- lib/app/features/movies/presentation/pages/movies_page.dart
+++ lib/app/features/movies/presentation/pages/movies_page.dart
@@ -1,7 +1,6 @@
 import 'package:coolmovies/app/features/movies/presentation/blocs/movies/movie_bloc.dart';
 import 'package:coolmovies/app/features/movies/presentation/blocs/movies/movie_event.dart';
 import 'package:coolmovies/app/features/movies/presentation/widgets/movie_card_widget.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -33,19 +32,13 @@ class MoviesPage extends HookWidget {
         builder: (_, state) {
           return state.maybeWhen(
             loadSuccess: (data) {
-              final list = [
-                ...data,
-                ...data.reversed,
-                ...data,
-                ...data.reversed
-              ];
               return CustomScrollView(
                 slivers: [
                   SliverPadding(
                     padding: const EdgeInsets.symmetric(vertical: 0),
                     sliver: SliverGrid.count(
                       crossAxisCount: 2,
-                      children: list
+                      children: data
                           .map(
                             (movie) => MovieCardWidget(
                               movieEntity: movie,
